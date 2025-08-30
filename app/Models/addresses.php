a2eae6d7f2a071006fd6f6e63cd44eb7b6dc5bf4
Malal Diallo <mm.diallo4@isepdiamniadio.edu.sn>
2025-08-30 00:15:31 +0000

    Ndewe Pain

---
+++ app/Models/addresses.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class addresses extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'ligne_adresse',
+        'ville',
+        'code_postal',
+        'pays',
+        'est_principale'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
---
+++ database/migrations/2025_08_29_235234_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('ligne_adresse');
+            $table->string('ville');
+            $table->string('code_postal');
+            $table->string('pays')->default('Sénégal');
+            $table->boolean('est_principale')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
